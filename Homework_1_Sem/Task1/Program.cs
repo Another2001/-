a4ad8b09a68208ddead8ff36505e6230a29498f7
A_not_her <sar.den.art2001@ya.ru>
2022-08-06 21:38:02 +0800

    complete 1 task

--- Homework_1_Sem/Task1/Program.cs
+++ Homework_1_Sem/Task1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//examples:
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите на проверку 2 числа:");
+Console.WriteLine("1 число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("2 число: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a > b) {
+    Console.WriteLine("a = " + a + "; b = "+ b + "-> max = " + a);
+}
+else if (b > a){
+    Console.WriteLine("a = " + a + "; b = "+ b + "-> max = " + b); 
+} else {
+    Console.WriteLine("chisla ravny!!" );
+}

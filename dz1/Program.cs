95a13276d8c774b5ef57c63e32a2a6710efaa387
A_not_her <sar.den.art2001@ya.ru>
2022-08-06 20:34:49 +0800

    решена 1 задача

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+//a = 25, b = 5 -> да 
+//a = 2, b = 10 -> нет 
+//a = 9, b = -3 -> да 
+//a = -3 b = 9 -> нет
+//
+//3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//	3 -> Среда 
+//5 -> Пятница
+double a,b;
+Console.WriteLine("Vvedite na proverku 2 chisla:");
+Console.WriteLine("1 chislo: ");
+Console.Readline(a);
+Console.WriteLine("2 chislo: ");
+Console.Readline(b);
+if (a==b*b) {
+Console.WriteLine("da"); //, pervoe chislo -" + a + "yavlyaetsya kvadratom vtorogo chisla -  + b
+}
+else {
+Console.WriteLine("net"); //, pervoe chislo -" + a + "yavlyaetsya kvadratom vtorogo chisla - " + b  
+}

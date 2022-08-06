9a167f68ea89f94fb59a5153c1e7211a1aae130f
A_not_her <sar.den.art2001@ya.ru>
2022-08-06 21:08:10 +0800

    change name from dz1 to seminar1

---
+++ seminar1/Program.cs
@@ -0,0 +1,21 @@
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+//a = 25, b = 5 -> да 
+//a = 2, b = 10 -> нет 
+//a = 9, b = -3 -> да 
+//a = -3 b = 9 -> нет
+//
+//3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//	3 -> Среда 
+//5 -> Пятница
+
+Console.WriteLine("Введите на проверку 2 числа:");
+Console.WriteLine("1 число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("2 число: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a == b*b) {
+Console.WriteLine("Да! "); //, pervoe chislo -" + a + "yavlyaetsya kvadratom vtorogo chisla -  + b
+}
+else {
+Console.WriteLine("Нет! "); //, pervoe chislo -" + a + "yavlyaetsya kvadratom vtorogo chisla - " + b  
+}

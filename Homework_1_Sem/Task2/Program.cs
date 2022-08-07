67036989d91cf6508036ff806a4bf7b8656146f1
A_not_her <sar.den.art2001@ya.ru>
2022-08-07 16:52:24 +0800

    complete 2 Task

--- Homework_1_Sem/Task2/Program.cs
+++ Homework_1_Sem/Task2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+int a = 70;int b = 25;int c = -99;int max = a;
+if (b > max){
+    max = b;
+}
+if(c > max){
+    max = c;
+}
+Console.Write(max);

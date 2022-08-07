cbc10e3387fd84e6d166b0e7d30e41b2319aa9ef
A_not_her <sar.den.art2001@ya.ru>
2022-08-07 17:54:25 +0800

    complete 4 task

--- Homework_1_Sem/Task4/Program.cs
+++ Homework_1_Sem/Task4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Random rnd = new Random();
+int N = rnd.Next(1, 25);
+int m = 1;
+Console.WriteLine("Введенное число : " + N);
+while (N>m){
+   m++;
+   if(m %2 == 0) {
+    Console.WriteLine(m);
+   } 
+}
+

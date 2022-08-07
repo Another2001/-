12342d8bb141751965d743f7e862a7b18efbad7c
A_not_her <sar.den.art2001@ya.ru>
2022-08-07 20:55:06 +0800

    решали задачи из семинара 2!

---
+++ seminar_2/Program.cs
@@ -0,0 +1,23 @@
+﻿//1. 11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+//звука нет) да, не очень) c# все же правильно? надо теперь это строкой как там эти процедуры 
+//теперь вторую?
+int random(){
+Random rnd = new Random();
+int a = rnd.Next(100, 999);  
+return a;  
+}
+int razdel(int a){
+int x = a%10;
+int y = a/100;  
+return y*10+x;  
+}
+void vivod(int a,int z){
+Console.WriteLine(" случайное число : " + a);
+Console.WriteLine(z);    
+}
+int a = random();
+int z = razdel(a);
+vivod(a,z);

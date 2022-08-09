c35299f6fe1b2503ff9e145f5d98cd7a3a4ddc89
A_not_her <sar.den.art2001@ya.ru>
2022-08-09 16:16:40 +0800

    end 1 task

--- homework_2_Sem/Task1/Program.cs
+++ homework_2_Sem/Task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+int random(){
+Random rnd = new Random();
+int a = rnd.Next(100, 999);  
+return a;  
+}
+int a = random();
+Console.WriteLine("Наше число : " + a );
+int secoNum(){
+   int x = a / 10;
+   x = x % 10;
+return x;
+}
+Console.WriteLine("Вторая цифра 3-х значного числа : " +secoNum());

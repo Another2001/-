c253388da657e824d5b7cb9d1e1ce65ffb82c765
A_not_her <sar.den.art2001@ya.ru>
2022-08-14 15:04:20 +0800

    /

--- seminar_3/task4/Program.cs
+++ seminar_3/task4/Program.cs
@@ -11,20 +11,20 @@ int y1 = Convert.ToInt32(Console.ReadLine());
 double AB = Math.Sqrt(((y - x)*(y - x)) + ((y1 - x1)*(y1 - x1)));    
 Console.Write("расстояние между 2 точек = " + AB);*/
 
+int n = 0, m = 0 , n1 = 0 , m1 = 0, p = 0;
 int vvod(){
 p = Convert.ToInt32(Console.ReadLine());
 return p;
 }
-int m = 0 , n1 = 0 , m1 = 0;
 var cortezh = (n = vvod(), n1 = vvod() );
 var cortezh1 = ( m = vvod(),m1 = vvod() );
 var dlina = Math.Sqrt((cortezh.Item2 - cortezh.Item1)+(cortezh1.Item2 - cortezh1.Item1));
 Console.Write("расстояние между 2 точек = " + dlina);
 
 //следующая
-int n = 6;
-int m = 1;
-for (int i = 1; i<=n; i++){
+int l = 6;
+int d = 1;
+for (int i = 1; i<=l; i++){
     m = i*i;
-    Console.Write(" - " + m);
+    Console.Write(" - " + d);
 }

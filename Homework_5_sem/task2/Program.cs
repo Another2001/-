6336ac9ac24d6f49a214f958e652853a39466fc3
A_not_her <sar.den.art2001@ya.ru>
2022-08-24 18:05:04 +0800

    Решено 5 Дз

--- Homework_5_sem/task2/Program.cs
+++ Homework_5_sem/task2/Program.cs
@@ -1,4 +1,38 @@
 ﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
-Console.WriteLine("Hello, World!");
+
+Console.Write("Введите размер массива : ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[a];
+void GetMass(int a)
+{
+    for (int i = 0; i < array.Length; i++)         
+    {
+        if (i % 2 == 1)
+        {
+            array[i] = new Random().Next(-25, 75);
+            Console.Write(" (" + array[i] + ") ");  // выделим нечетные элементы
+        } else
+        {
+            array[i] = new Random().Next(-25, 75);
+            Console.Write(" " + array[i] + " ");
+        }
+        
+    }
+}
+int Sum()
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+GetMass(a);
+Console.WriteLine($"Сумма нечетных элементов = {Sum()}");
--- Homework_5_sem/task3/Program.cs
+++ Homework_5_sem/task3/Program.cs
@@ -1,4 +1,46 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
-
+Console.Write("Введите размер массива : ");
+int a = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[a];
+void GetMasDou(int a)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round(new Random().NextDouble() * 100 - 50, 2);
+        Console.Write(" " + array[i] + " ");
+    }
+}
+double Min()
+{
+    double mini = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < mini)
+        {
+            mini = array[i];
+        }
+    }
+    return mini;
+}
+double Max()
+{
+    double maxi = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > maxi)
+        {
+            maxi = array[i];
+        }
+    }
+    return maxi;
+}
+double Differ()
+{
+    double diff = Math.Round(Max() - Min(), 2);
+    return diff;
+}
+GetMasDou(a);
+Console.WriteLine();
+Console.WriteLine($"Разница между мин. и макс. элементами массива = {Differ()}");

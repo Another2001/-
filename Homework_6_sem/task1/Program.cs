b1e8ff09ab560ccc92bf6a241f03ea7a4a06ef04
A_not_her <sar.den.art2001@ya.ru>
2022-08-26 20:23:27 +0800

    решено 6 дз

--- Homework_6_sem/task1/Program.cs
+++ Homework_6_sem/task1/Program.cs
@@ -2,3 +2,25 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+// совершенно не понимаю как тут двумерные массивы привязывать к этой задаче, 
+// ведь по сути даже и одномерный массив то не нужен)
+
+Console.Write("Введите кол-во вводимых чисел : ");
+int m = Convert.ToInt32(Console.ReadLine());
+int CountMoreThanZero(int m)
+{
+    int count = 0;
+    Console.Write("Наш набор чисел : ");
+    for (int i = 0; i < m; i++)
+    {
+        int chislo = new Random().Next(-5, 10);
+        Console.Write($" {chislo}");
+        if (chislo > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine("");
+    return count;
+}
+Console.WriteLine($"Чисел больше 0 -> {CountMoreThanZero((m))}");
--- Homework_6_sem/task3_hard/Program.cs
+++ Homework_6_sem/task3_hard/Program.cs
@@ -1,2 +1,70 @@
-﻿// Задача для любознательных
+﻿// pадача со звездочкой
 // ***Написать алгоритм поворота матрицы [N;N] на 90 градусов
+
+Console.Write("Введите размерность матрицы :");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[N, N];
+int[,] firstmat = new int[N, N];
+
+int[,] CreateMatrix(int[,] matrix)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+            Console.Write($" {matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
+
+firstmat = matrix;
+
+int[,] RotateMatLeft(int[,] matrix)
+{
+    for (int j = firstmat.GetLength(1) - 1; j >= 0; j--)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            Console.Write($" {firstmat[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return firstmat;
+}
+
+int[,] RotateMatRight(int[,] matrix)
+{
+    for (int j = 0; j < N; j++)
+    {
+        for (int i = firstmat.GetLength(0) - 1; i >= 0; i--)
+        {
+            Console.Write($" {firstmat[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return firstmat;
+}
+int[,] TurnOverMat(int[,] matrix)                                  //переворачиваем
+{
+    for (int i = firstmat.GetLength(0) - 1; i >= 0; i--)
+    {
+        for (int j = firstmat.GetLength(1) - 1; j >= 0; j--)
+        {
+            Console.Write($" {firstmat[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return firstmat;
+}
+
+CreateMatrix(matrix);
+Console.WriteLine("повернутая налево матрица  : ");
+RotateMatLeft(firstmat);
+Console.WriteLine("повернутая направо матрица  : ");
+RotateMatRight(firstmat);
+Console.WriteLine("перевернутая матрица  : ");
+TurnOverMat(firstmat);

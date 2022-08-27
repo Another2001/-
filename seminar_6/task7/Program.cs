03fd66f7da9c2139e1960c6a09b8a2be0dad0639
A_not_her <sar.den.art2001@ya.ru>
2022-08-27 12:07:55 +0800

    вывести множители чисел из периода 1 до N

---
+++ seminar_6/task7/Program.cs
@@ -0,0 +1,15 @@
+﻿// вывести все множители каждого числа от 1 до N
+Console.Write("введите количество чисел для определения: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    Console.Write($"число: {i} делители - ");
+    for (int j = 1; j <= N; j++)
+    {
+        if (i % j == 0)
+        {
+            Console.Write($" {j}; ");
+        }
+    }
+    Console.WriteLine();
+}

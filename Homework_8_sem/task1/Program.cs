2a8e1e92280fbeb67311f3ae7d73990ea4fc843b
A_not_her <sar.den.art2001@ya.ru>
2022-08-31 14:52:13 +0800

    промежуточный рез-т решения 8 ДЗ

--- Homework_8_sem/task1/Program.cs
+++ Homework_8_sem/task1/Program.cs
@@ -7,3 +7,79 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+int[,] CreateArray(int rows, int columns)
+{
+    return new int[rows, columns];
+}
+void Fill(int[,] array, int minValue, int maxValue)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int row = 0; row < rows; row++)
+    {
+        for (int column = 0; column < columns; column++)
+        {
+            array[row, column] = new Random().Next(minValue, maxValue);
+        }
+    }
+}
+void Print(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int row = 0; row < rows; row++)
+    {
+        for (int column = 0; column < columns; column++)
+        {
+            Console.Write($"{array[row, column],5}");
+        }
+        Console.WriteLine();
+    }
+}
+int Input(string text)
+{
+    bool f = false;
+    int number = 0;
+    while (!f)
+    {
+        Console.Write(text);
+        string str = Console.ReadLine();
+        f = int.TryParse(str, out number);
+    }
+    return number;
+}
+int[,] Sorting(int[,] array)
+{
+    int min = 0;
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        min = array[row,0];
+        for (int col = 0; col <= array.GetLength(1) - 1; col++)
+        {
+            if (array[row, col] > min)
+            {
+                min = array[row, col];
+                (array[row, col ], array[row, col + 1]) = (array[row, col + 1], array[row, col]);   
+            }
+            Console.Write($"{array[row, col],5} ");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+
+int m = Input("rows:");
+int n = Input("columns:");
+
+if (m >= 0 && n >= 0)
+{
+    int[,] matrix = CreateArray(m, n);
+    Fill(matrix, 10, 20);
+    Print(matrix);
+    Console.WriteLine();
+    Sorting(matrix);
+}
--- Homework_8_sem/task2/Program.cs
+++ Homework_8_sem/task2/Program.cs
@@ -5,3 +5,78 @@
 // 8 4 2 4
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] CreateArray(int rows, int columns)
+{
+    return new int[rows, columns];
+}
+void Fill(int[,] array, int minValue, int maxValue)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int row = 0; row < rows; row++)
+    {
+        for (int column = 0; column < columns; column++)
+        {
+            array[row, column] = new Random().Next(minValue, maxValue);
+        }
+    }
+}
+void Print(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int row = 0; row < rows; row++)
+    {
+        for (int column = 0; column < columns; column++)
+        {
+            Console.Write($"{array[row, column],5}");
+        }
+        Console.WriteLine();
+    }
+}
+int Input(string text)
+{
+    bool f = false;
+    int number = 0;
+    while (!f)
+    {
+        Console.Write(text);
+        string str = Console.ReadLine();
+        f = int.TryParse(str, out number);
+    }
+    return number;
+}
+(int, int) RowsSum(int[,] array)
+{
+    int X = 0;
+    int maxsum = 0;
+    int sum = 0;
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        sum = 0;
+        for (int col = 0; col < array.GetLength(1); col++)
+        {
+            sum += array[row, col];
+        }
+        if (sum > maxsum)
+        {
+            X = row;
+            maxsum = sum;
+        }
+    }
+    return (maxsum, +X);
+}
+
+int m = Input("кол - во строк: ");
+int n = Input("кол - во стобиков: ");
+
+if (m >= 0 && n >= 0)
+{
+    int[,] matrix = CreateArray(m, n);
+    Fill(matrix, 5, 15);
+    Print(matrix);
+    Console.WriteLine("Cумма строки " + RowsSum(matrix).Item1 + ", номер строки с макс. суммой: " + (RowsSum(matrix).Item2 + 1));
+}

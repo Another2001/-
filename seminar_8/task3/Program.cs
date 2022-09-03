be8e09b6f18aeb410144e73c8984beb7ee7addc4
A_not_her <sar.den.art2001@ya.ru>
2022-09-03 20:48:37 +0800

    задачка из ЕГЭ

---
+++ seminar_8/task3/Program.cs
@@ -0,0 +1,35 @@
+﻿// // сколько существует программ
+// int k = 2;
+// int FunctionOne(int k)
+// {
+//  k += 1; 
+//  return k;
+// }
+// int FunctionTwo(int k)
+// {
+//  k *= 2; 
+//  return k;
+// }
+
+// for (int i = 0; i < 10; i++)
+// {   
+//     int count = 0;
+//     while (k<=7) {
+// }
+
+// }
+// 2*2+1+1+1
+// (2+1)*2+1
+// 2+1+1+1+1+1
+
+
+
+int func(int x, int y)
+{ 
+    int t = 0;
+    if (x == y) t = 1 ;
+    if (x > y) t = 0 ;
+    if(x < y) t = func(x+1,y) + func(x*2,y);
+    return t;
+}
+Console.WriteLine(func(2,7));
